e9a96cd086496881d78649018e8f899681675400
prajyot burbure <prajyot.burbure@kloudspot.com>
2023-08-06 21:09:07 +0530

    build small app for teleparty

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,26 +7,34 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
 
+
+const Layout = ({ children, title = 'Home Page' }: Props) =>{ 
+  //year
+  const d = new Date();
+  let year = d.getFullYear();
+  
+  return (
+    <div className='w-full h-full'>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+      <header className='w-full h-full py-10'>
+        <Link href={'/'} >
+          <h1 className='mx-auto font- text-3xl text-center text-white'>Teleparty Assignment
+          </h1>
+        </Link>
+      </header>
+      {children}
+      <div className='py-10'>
+        <div className='text-center text-white'>
+            <h1 className='text-xl'>Prajyot<span className='text-black'>.</span></h1>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default Layout
